a0d4b8d1f0c4f6322639404afa4850b88c43b101
Tilak <tilaksharma1114@gmail.com>
2019-01-26 08:49:01 +0530

    signup added and update info added. Flow issue

--- app/src/main/java/com/example/tilak/pumpit/SignupFrag.java
+++ app/src/main/java/com/example/tilak/pumpit/SignupFrag.java
@@ -13,7 +13,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 
@@ -21,11 +33,16 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SignupFrag extends Fragment {
     private static final int CHOOSE_IMAGE = 101;
-    Button signupadmin;
+    Button signupadmin, subSignUp;
     Dialog snpDialog;
+    String profileImgUrl;
+    EditText gymName, gymEmail, gymPwd, gymPh, gymAddr;
     ImageView cancelSnp;
     CircleImageView Avatar;
     Uri uriProfileImage;
+
+    FirebaseAuth mAuth;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,8 +50,16 @@ public class SignupFrag extends Fragment {
         signupadmin = signup.findViewById(R.id.newadmin);
         snpDialog = new Dialog(signup.getContext(), android.R.style.Theme_Light_NoTitleBar);
         snpDialog.setContentView(R.layout.signup_dialog);
+        subSignUp = snpDialog.findViewById(R.id.subsnp);
         cancelSnp = snpDialog.findViewById(R.id.cancelSignUp);
+        gymName = snpDialog.findViewById(R.id.snpName);
+        gymEmail = snpDialog.findViewById(R.id.adminemail);
+        gymPwd = snpDialog.findViewById(R.id.adminpwd);
+
         Avatar = snpDialog.findViewById(R.id.snpavatar);
+
+        mAuth = FirebaseAuth.getInstance();
+
         return signup;
     }
 
@@ -59,7 +84,25 @@ public class SignupFrag extends Fragment {
                 showImageChooser();
             }
         });
+        subSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String GymEmail = gymEmail.getText().toString();
+                String GymPwd = gymPwd.getText().toString();
+                mAuth.createUserWithEmailAndPassword(GymEmail, GymPwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            uploadToFireBase();
+                            saveUserInfo();
+                            startActivity(new Intent(getActivity(), InAppActivity.class));
+                        }
+                    }
+                });
+            }
+        });
     }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -80,5 +123,34 @@ public class SignupFrag extends Fragment {
         imgSelect.setType("image/*");
         imgSelect.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(imgSelect, "Select Profile Image"), CHOOSE_IMAGE);
+    }
+    private void uploadToFireBase() {
+        final StorageReference profilepicRef = FirebaseStorage.getInstance()
+                .getReference("profilepic"+System.currentTimeMillis()+".jpg");
+        if(uriProfileImage != null){
+            profilepicRef.putFile(uriProfileImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    profileImgUrl = taskSnapshot.toString();
+                }
+            });
+        }
+    }
+    private void saveUserInfo() {
+        final String GymName = gymName.getText().toString();
+        //final String GymPh = gymPh.getText().toString();
+        //final String GymAddr = gymAddr.getText().toString();
+
+        if(GymName.isEmpty()){
+            gymName.setError("Name Required");
+            gymName.requestFocus();
+            return;
+        }
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null && profileImgUrl != null){
+            UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(GymName).setPhotoUri(Uri.parse(profileImgUrl)).build();
+            user.updateProfile(profileChangeRequest);
+        }
     }
 }
