74b15485408b7f4c0efcd41d81912b6e84d5110d
Tilak <tilaksharma1114@gmail.com>
2019-01-28 12:55:09 +0530

    signup added and update info added. Flow issue

--- app/src/main/java/com/example/tilak/pumpit/ManageMembersFragment.java
+++ app/src/main/java/com/example/tilak/pumpit/ManageMembersFragment.java
@@ -1,25 +1,82 @@
 package com.example.tilak.pumpit;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ManageMembersFragment extends Fragment {
+    EditText firstName, lastName, phoneNo;
+    Dialog addMembDialog;
+    RelativeLayout addNewMembFab;
+    private DocumentReference documentReference = FirebaseFirestore.getInstance().document("Gyms/MalakaFitness" +
+            "/Members/Member1");
+    Button addMembbtn;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle SavedInstanceState){
         View MemberManage = inflater.inflate(R.layout.fragment_manage_member, container, false);
+        addMembDialog = new Dialog(MemberManage.getContext(), android.R.style.Theme_Light_NoTitleBar);
+        addMembDialog.setContentView(R.layout.newmemb_popup);
+        firstName = addMembDialog.findViewById(R.id.newMembFN);
+        lastName = addMembDialog.findViewById(R.id.newMembLN);
+        phoneNo = addMembDialog.findViewById(R.id.newMembPNO);
+        addMembbtn = addMembDialog.findViewById(R.id.addnewmemb);
+        addNewMembFab = MemberManage.findViewById(R.id.addMembfab);
         return MemberManage;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
+        addNewMembFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMembDialog.show();
+            }
+        });
+        addMembbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String MemberFN, MemberLN, MemberPhno;
+                MemberFN = firstName.getText().toString();
+                MemberLN = lastName.getText().toString();
+                MemberPhno = phoneNo.getText().toString();
+                Map<String, Object> data = new HashMap<String, Object>();
+                data.put("firstName", MemberFN);
+                data.put("lastName", MemberLN);
+                data.put("phoneNo", MemberPhno);
+                documentReference.set(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getContext(), "Member Added", Toast.LENGTH_LONG).show();
+                            addMembDialog.dismiss();
+                        }
+                        else
+                            Toast.makeText(getContext(), "Failed", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tilak/pumpit/ManagePlansFragment.java
+++ app/src/main/java/com/example/tilak/pumpit/ManagePlansFragment.java
@@ -1,6 +1,8 @@
 package com.example.tilak.pumpit;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,13 +10,18 @@ import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.CardView;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -22,10 +29,16 @@ import android.widget.Toast;
 
 public class ManagePlansFragment extends Fragment {
     ScrollView scrollView;
+    LinearLayout planListLL;
     Dialog addplanDialog;
     RelativeLayout addplanfab;
-    GridView gridview;
     ImageView cancelpopup;
+    LinearLayout.LayoutParams layoutparams;
+    Button addplanbtn;
+    EditText planDur, planPrice;
+    TextView newPlan, newPlanMembers;
+    CardView cardView;
+    Context context= getContext();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle SavedInstanceState){
@@ -33,11 +46,13 @@ public class ManagePlansFragment extends Fragment {
         scrollView = mngplans.findViewById(R.id.plansscroll);
         addplanDialog = new Dialog(mngplans.getContext(), android.R.style.Theme_Light_NoTitleBar);
         addplanDialog.setContentView(R.layout.addplan_popup);
+        planListLL = mngplans.findViewById(R.id.planlistll);
+        planDur = addplanDialog.findViewById(R.id.plnmonths);
+        planPrice = addplanDialog.findViewById(R.id.planPrice);
         cancelpopup = addplanDialog.findViewById(R.id.cancelplanpopup);
+        addplanbtn = addplanDialog.findViewById(R.id.addnewplan);
         addplanfab = mngplans.findViewById(R.id.addplanfab);
 
-        gridview = addplanDialog.findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(getContext()));
         return mngplans;
     }
 
@@ -51,11 +66,53 @@ public class ManagePlansFragment extends Fragment {
                 addplanDialog.show();
             }
         });
+
+        addplanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String PlanDur = planDur.getText().toString();
+                createDynamicCardView(PlanDur+" Months Plan", "23 Members");
+            }
+        });
+
         cancelpopup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addplanDialog.dismiss();
             }
         });
+    }
+
+    private void createDynamicCardView(String planName, String planMembers) {
+        cardView = new CardView(context);
+
+        layoutparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        cardView.setLayoutParams(layoutparams);
+
+        newPlan = new TextView(context);
+        newPlanMembers = new TextView(context);
+
+        newPlan.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        newPlanMembers.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        newPlan.setText(planName);
+        newPlanMembers.setText(planMembers);
+
+        newPlanMembers.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+        newPlan.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+
+        newPlan.setTextColor(Color.WHITE);
+
+
+        newPlan.setGravity(Gravity.BOTTOM);
+
+        cardView.addView(newPlan);
+
+        planListLL.addView(cardView);
     }
 }
