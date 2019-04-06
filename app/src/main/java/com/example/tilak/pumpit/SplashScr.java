0180a6df78708f0939d72f68f65572a2564fda39
Tilak <tilaksharma1114@gmail.com>
2019-04-06 07:18:00 +0530

    Splashscr second attempt

--- app/src/main/java/com/example/tilak/pumpit/SplashScr.java
+++ app/src/main/java/com/example/tilak/pumpit/SplashScr.java
@@ -3,13 +3,26 @@ package com.example.tilak.pumpit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScr extends AppCompatActivity {
-
+    ImageView img;
+    TextView tv1, tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_scr);
+        Animation animationFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_intwosec);
+        img = findViewById(R.id.pumpitimglogo);
+        tv1 = findViewById(R.id.splashtv1);
+        tv2 = findViewById(R.id.splashtv2);
+
+        img.startAnimation(animationFadeIn);
+        tv1.startAnimation(animationFadeIn);
+        tv2.startAnimation(animationFadeIn);
 
         Thread thread = new Thread(){
             public void run(){
